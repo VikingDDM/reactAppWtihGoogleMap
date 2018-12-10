494b6d07071add163f82f6cb3f35f2930f2705eb
VikingDDM <chrisdulin4ever@gmail.com>
2018-12-10 08:17:02 +0200

    Added shapes example to docz

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,4 +1,8 @@
 export const imports = {
   'src/docs/GoogleMap.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-docs-google-map" */ 'src/docs/GoogleMap.mdx'),
+  'src/docs/GroundOverlay.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-ground-overlay" */ 'src/docs/GroundOverlay.mdx'),
+  'src/docs/Shapes.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-shapes" */ 'src/docs/Shapes.mdx'),
 }
