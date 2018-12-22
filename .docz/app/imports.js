394ec62af917e266614b6f2ba7b1ebd8cfa2c1f0
VikingDDM <chrisdulin4ever@gmail.com>
2018-12-22 19:18:08 +1100

    Add Circle example to docz

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -7,12 +7,8 @@ export const imports = {
     import(/* webpackPrefetch: true, webpackChunkName: "src-docs-google-map" */ 'src/docs/GoogleMap.mdx'),
   'src/docs/GroundOverlay.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-docs-ground-overlay" */ 'src/docs/GroundOverlay.mdx'),
-  'src/docs/Marker.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-marker" */ 'src/docs/Marker.mdx'),
-  'src/docs/Polyline.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-polyline" */ 'src/docs/Polyline.mdx'),
-  'src/docs/Rectangle.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-rectangle" */ 'src/docs/Rectangle.mdx'),
+  'src/docs/Polygon.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-polygon" */ 'src/docs/Polygon.mdx'),
   'src/docs/Shapes.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-docs-shapes" */ 'src/docs/Shapes.mdx'),
 }
