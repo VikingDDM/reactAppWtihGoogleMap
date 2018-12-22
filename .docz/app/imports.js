7f9712f4f006eed86a752eba57e214182145ddd9
VikingDDM <chrisdulin4ever@gmail.com>
2018-12-22 19:18:08 +1100

    Add Circle example to docz

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,12 +1,12 @@
 export const imports = {
   'src/docs/BicyclingLayer.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-docs-bicycling-layer" */ 'src/docs/BicyclingLayer.mdx'),
+  'src/docs/Circle.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-circle" */ 'src/docs/Circle.mdx'),
   'src/docs/GoogleMap.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-docs-google-map" */ 'src/docs/GoogleMap.mdx'),
   'src/docs/GroundOverlay.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-docs-ground-overlay" */ 'src/docs/GroundOverlay.mdx'),
-  'src/docs/Polygon.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-polygon" */ 'src/docs/Polygon.mdx'),
   'src/docs/Shapes.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-docs-shapes" */ 'src/docs/Shapes.mdx'),
 }
