dee61dbbb9ba2e47d4e396c68d43614eba3ebb7d
VikingDDM <chrisdulin4ever@gmail.com>
2018-12-23 12:00:17 +1100

    Add `Marker` example to docz

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,10 +1,16 @@
 export const imports = {
-  'src/docs/GoogleMap.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-google-map" */ 'src/docs/GoogleMap.mdx'),
   'src/docs/BicyclingLayer.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-docs-bicycling-layer" */ 'src/docs/BicyclingLayer.mdx'),
+  'src/docs/Circle.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-circle" */ 'src/docs/Circle.mdx'),
+  'src/docs/GoogleMap.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-google-map" */ 'src/docs/GoogleMap.mdx'),
   'src/docs/GroundOverlay.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-docs-ground-overlay" */ 'src/docs/GroundOverlay.mdx'),
+  'src/docs/Marker.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-marker" */ 'src/docs/Marker.mdx'),
+  'src/docs/Polyline.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-polyline" */ 'src/docs/Polyline.mdx'),
   'src/docs/Shapes.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-docs-shapes" */ 'src/docs/Shapes.mdx'),
 }
